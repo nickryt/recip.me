5e93d6847d489dfe6f832e6efda26bc5c2d87d65
Nick <rytnick@gmail.com>
2020-10-04 12:44:54 -0400

    Updated Commit

--- app/src/main/java/com/example/recipme/Database.java
+++ app/src/main/java/com/example/recipme/Database.java
@@ -4,7 +4,6 @@ import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.widget.Toast;
 
 import com.google.android.gms.tasks.Continuation;
 import com.google.android.gms.tasks.OnCompleteListener;
